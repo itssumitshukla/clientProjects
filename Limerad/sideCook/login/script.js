3b1c530ceed4a517178285f4eb8dd112c828d91d
itssumitshukla <itssumitshukla@gmail.com>
2021-05-28 21:39:28 -1000

    setup event listner on login page

---
+++ Limerad/sideCook/login/script.js
@@ -0,0 +1,7 @@
+//
+let registerr = document.getElementById('register');
+registerr.addEventListener('click', loadPage);
+
+function loadPage(){
+ window.location.href = '/register/register.html';
+};
